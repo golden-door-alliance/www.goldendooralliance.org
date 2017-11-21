47730e5c8a2bb112d7b26d9fc3a021516ac13597
Michael Kaiser-Nyman <michaelrkn@gmail.com>
2017-11-20 20:11:41 -0600

    add tel links to phone numbers

--- scripts.js
+++ scripts.js
@@ -1,10 +1,15 @@
 var url = new URL(location.href);
 for(var key of url.searchParams.keys()) {
   if (key) {
-    console.log(key);
     var el = document.querySelector('#' + key);
+    var val = url.searchParams.get(key);
     if (el) {
-      el.innerHTML = url.searchParams.get(key);
+      el.innerHTML = val;
+    }
+    var phoneLink = document.querySelector('#' + key + 'Link');
+    if (phoneLink) {
+      var phone = val.replace("(", "").replace(")", "").replace(" ", "").replace("-", "");
+      phoneLink.setAttribute('href', 'tel:+1' + phone);
     }
   }
 }
